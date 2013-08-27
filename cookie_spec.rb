edb23fc003a9280c7182925b220745a899e5d6e8
dbcapprentice <dbcapprentice@dbc.com>
2013-08-27 14:53:58 -0700

    finished writing test

--- cookie_spec.rb
+++ cookie_spec.rb
@@ -22,36 +22,85 @@ describe Cookie do
   end
 
   describe "#type" do
-    it "returns the type of the cookie"
+    it "returns the type of the cookie" do
+      cookie.type.should eq 'peanut butter'
+    end
   end
 
   describe "#bake!" do
-    it "requires an integer time argument"
+    it "requires an integer time argument" do
+      expect {cookie.bake!()}.to raise_error(ArgumentError)
+    end
 
-    it "returns the cookie object"
+    it "returns the cookie object" do
+      cookie.should eq cookie
+    end
+
+    it "changes the status of the cookie when given enough time" do
+      expect { cookie.bake!(7) }.to change(cookie, :status)
+    end
 
     it "changes the status of the cookie when given enough time" do
       expect { cookie.bake!(10) }.to change(cookie, :status)
     end
+
+    it "changes the status of the cookie when given enough time" do
+      expect { cookie.bake!(12) }.to change(cookie, :status)
+    end
+s
+    it "changes the status of the cookie when given enough time" do
+      expect { cookie.bake!(13) }.to change(cookie, :status)
+    end
+
   end
 
-  describe "#status" do
-    it "returns the cookie's current status"
 
-    context "when unbaked" do
-      it "is `:doughy`" 
+  describe "#status" do
+    it "returns the cookie's current status" do
+      cookie.status.should eq :doughy
     end
 
     context "when baked for less than 7 minutes" do
-      it "is `:doughy`"
-    end  
+      (0..6).to_a.each do |time|
+        it "returns doughy if time is #{time}" do
+          cookie.bake!(time)
+          cookie.status.should eq :doughy
+        end
+      end
+    end
 
     context "when baked for at least 7 but less than 10 minutes" do
-      it "is `:almost_ready`"
+      (7..9).to_a.each do |time|
+        it 'returns almost_ready if time is #{time}' do
+        cookie.bake!(time)
+        cookie.status.should eq :almost_ready
+        end
+      end
+    end
+
+    context "when baked for at least 7 but less than 10 minutes" do
+      (7..9).to_a.sample do |time|
+        it 'returns almost_ready if time is #{time}' do
+        cookie.bake!(time)
+        cookie.status.should eq :almost_ready
+        end
+      end
+    end
+
+    context "when baked for at least 7 but less than 10 minutes" do
+        it 'returns almost_ready if time is between 7..9' do
+        cookie.bake!(rand(7..9))
+        cookie.status.should eq :almost_ready
+      end
     end
 
     context "when baked for at least 10 but less than 12 minutes" do
-      it "is `:ready`"
+        (10..11).to_a.each do |time|
+      it 'returns ready if the time is #{time}' do
+        cookie.bake!(time)
+        cookie.status.should eq :ready
+        end
+      end
     end
 
     context "when baked for at least 12 minutes" do
